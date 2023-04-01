fa157cad8e574c7be31604b58a0bf30b02149b12
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 16:01:07 +0200

    automated commit by makeTag.sh

--- makeTag.sh
+++ makeTag.sh
@@ -20,7 +20,9 @@ export HOMEBREW_EDITOR="/bin/cat"
 
 for file in *; do
   if [[ $file != "README.md" && $file != "src" && ${file: -3} != ".sh" ]]; then
+	  rm /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/$file.rb
     brew create https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/$latest_tag.tar.gz --set-name=$file > ../homebrew-jogo/$file.rb
+  rm /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/$file.rb
   fi
 done
 
